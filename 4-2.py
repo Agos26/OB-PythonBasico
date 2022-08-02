029d0715238a528b11558d24ff046da918dcfdc0
Agostina <agos.b.sanchez@gmai.com>
2022-08-02 18:50:44 -0300

    Correcion de codigo

--- 4-2.py
+++ 4-2.py
@@ -5,8 +5,6 @@ Por ejemplo: teniendo numero_inicial = 2 y numero_final = 8,
 el programa debe imprimir por consola: [3, 5, 7] """
 lista = [2,4,5,6,7,8,9,10]
 for i in lista:
-    if i% 2  == 0:
-      pass
-    else:
+    if i%2  != 0:
         print(i)
    
