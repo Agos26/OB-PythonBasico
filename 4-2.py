247e5a85f83eb921fb7450dcfa58418b9d75ad58
Agostina <agos.b.sanchez@gmai.com>
2022-08-02 18:47:31 -0300

    Ej 4 parte 2

---
+++ 4-2.py
@@ -0,0 +1,12 @@
+""" Escribe un programa capaz de mostrar todos los números impares desde un número de inicio
+ y otro final.
+
+Por ejemplo: teniendo numero_inicial = 2 y numero_final = 8, 
+el programa debe imprimir por consola: [3, 5, 7] """
+lista = [2,4,5,6,7,8,9,10]
+for i in lista:
+    if i% 2  == 0:
+      pass
+    else:
+        print(i)
+   
